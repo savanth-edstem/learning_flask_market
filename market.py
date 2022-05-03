bcc7c4a18f27853f38f37a62a2e42d9d93316caa
savanth-edstem <savanth.s@edstem.com>
2022-05-03 12:41:05 +0530

    elements added to data base

--- market.py
+++ market.py
@@ -35,9 +35,10 @@ def about_page(username):
 
 @app.route('/market')
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    items = Item.query.all()
+    # items = [
+    #     # {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
+    #     # {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
+    #     # {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
+    # ]
     return render_template("market.html", items=items)
