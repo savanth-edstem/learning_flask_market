c4f9932260f8a1520a3f12b8423d120af8854252
savanth-edstem <savanth.s@edstem.com>
2022-05-04 15:39:18 +0530

    register page created

--- market/routes.py
+++ market/routes.py
@@ -1,25 +1,33 @@
 from market import app
 from flask import render_template
 from market.models import Item
+from market.forms import RegisterForm
+
+
+# @app.route('/')
+# def hello_world():
+#     return "<h1>Hello world...</h1>"
 
 
 @app.route('/')
-def hello_world():
-    return "<h1>Hello world...</h1>"
-
-
-@app.route('/home')
-@app.route("/home2")
+@app.route("/home")
 def home_page():
     return render_template("home.html")
 
 
-@app.route('/about/<username>')  # dynamic route
-def about_page(username):
-    return f"<h1>This is about page of {username}</h>"
+# this is a dynamic route
+# @app.route('/about/<username>')
+# def about_page(username):
+#     return f"<h1>This is about page of {username}</h>"
 
 
 @app.route('/market')
 def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
+
+
+@app.route('/register')
+def register_page():
+    form = RegisterForm()
+    return render_template('register.html', form=form)
