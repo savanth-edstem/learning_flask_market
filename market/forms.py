a91eb4c8f8b1ae8c7616e83a07569873768099b1
savanth-edstem <savanth.s@edstem.com>
2022-05-06 11:36:51 +0530

    validators corrected

--- market/forms.py
+++ market/forms.py
@@ -6,6 +6,6 @@ from wtforms.validators import Length, equal_to, Email, DataRequired
 class RegisterForm(FlaskForm):
     username = StringField(label='User Name', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email', validators=[Email(), DataRequired()])
-    password1 = PasswordField(label='Password', validators=[Length(min=6), 'DataRequired()'])
+    password1 = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Conform Password', validators=[equal_to("password1"), DataRequired()])
     submit = SubmitField(label='Create Account')
