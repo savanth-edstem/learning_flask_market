da7bacfb830236e24bcec9f8fb6c9101f119097f
savanth-edstem <savanth.s@edstem.com>
2022-05-06 11:04:47 +0530

    validators created

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///market.db"
-app.config['SECRET_KEY']='01d88c32055f2d87fe83cb76'
+app.config['SECRET_KEY'] = '01d88c32055f2d87fe83cb76'
 db = SQLAlchemy(app)
 from market import routes
--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Length, equal_to, Email, DataRequired
 
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='User Name')
-    email_address = StringField(label='Email')
-    password1 = PasswordField(label='Password')
-    password2 = PasswordField(label='Conform Password')
+    username = StringField(label='User Name', validators=[Length(min=2, max=30), DataRequired()])
+    email_address = StringField(label='Email', validators=[Email(), DataRequired()])
+    password1 = PasswordField(label='Password', validators=[Length(min=6), 'DataRequired()'])
+    password2 = PasswordField(label='Conform Password', validators=[equal_to("password1"), DataRequired()])
     submit = SubmitField(label='Create Account')
