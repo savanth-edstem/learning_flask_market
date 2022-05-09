acd9e91ed8031cc9e2ef7e54e638ae9c34e5c5da
savanth-edstem <savanth.s@edstem.com>
2022-05-09 12:36:43 +0530

    register page error display compleated

--- market/forms.py
+++ market/forms.py
@@ -1,9 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import Length, equal_to, Email, DataRequired
+from wtforms.validators import Length, equal_to, Email, DataRequired, ValidationError
+from market.models import User
 
 
 class RegisterForm(FlaskForm):
+
+    def validate_username(self, username_to_check):
+        user = User.query.filter_by(username=username_to_check.data).first()
+        if user:
+            raise ValidationError('username already exists please use another username')
+
+    def validate_email_address(self, email_address_to_check):
+        email_address = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email_address:
+            raise ValidationError('Email already exists try a new one')
+
     username = StringField(label='User Name', validators=[Length(min=2, max=30), DataRequired()])
     email_address = StringField(label='Email', validators=[Email(), DataRequired()])
     password1 = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
