35e2e506e8bab1935c244e399e5c41482ff8a4af
savanth-edstem <savanth.s@edstem.com>
2022-05-09 15:33:09 +0530

    Login page created

--- market/__init__.py
+++ market/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///market.db"
 app.config['SECRET_KEY'] = '01d88c32055f2d87fe83cb76'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 from market import routes
--- market/forms.py
+++ market/forms.py
@@ -21,3 +21,10 @@ class RegisterForm(FlaskForm):
     password1 = PasswordField(label='Password', validators=[Length(min=6), DataRequired()])
     password2 = PasswordField(label='Conform Password', validators=[equal_to("password1"), DataRequired()])
     submit = SubmitField(label='Create Account')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(label='User Name', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Sign In')
+
