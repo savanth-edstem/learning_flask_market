0b56d465d34cca3df33473adba33708acefe6f90
savanth-edstem <savanth.s@edstem.com>
2022-05-10 17:45:54 +0530

    login page entry compleated

--- market/__init__.py
+++ market/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///market.db"
 app.config['SECRET_KEY'] = '01d88c32055f2d87fe83cb76'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 from market import routes
--- market/models.py
+++ market/models.py
@@ -1,9 +1,14 @@
-import flask_bcrypt
+from market import db, login_manager
+from flask_login import UserMixin
+from market import bcrypt
 
-from market import db
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50), nullable=False, unique=True)
@@ -16,8 +21,11 @@ class User(db.Model):
         return self.password
 
     @password.setter
-    def password(self,plain_text_password):
-        self.password_hash = flask_bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+    def password(self, plain_text_password):
+        self.password_hash = bcrypt.generate_password_hash(plain_text_password).decode('utf-8')
+
+    def check_password_correction(self, attempted_password):
+        return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
 
 class Item(db.Model):
