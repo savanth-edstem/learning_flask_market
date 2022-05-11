1eb3fb28462a76fdb6380f5306f01b5455f410cd
savanth-edstem <savanth.s@edstem.com>
2022-05-11 15:41:33 +0530

    logout compleated

--- market/routes.py
+++ market/routes.py
@@ -3,7 +3,7 @@ from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
 from market.forms import RegisterForm, LoginForm
 from market import db
-from flask_login import login_user
+from flask_login import login_user,logout_user
 
 
 # @app.route('/')
@@ -58,3 +58,11 @@ def login_page():
             flash("Username and password are not match! please try again", category='danger')
 
     return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("you have been logged out!",category="info")
+    return redirect(url_for("home_page"))
+
